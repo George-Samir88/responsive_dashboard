18ec889180db259c448aabdafae91114c8d340c8
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-03-19 15:49:11 +0200

    all expenses part 2

--- lib/views/widgets/all_expenses_header.dart
+++ lib/views/widgets/all_expenses_header.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/core/utils/styles.dart';
+import 'package:responsive_dashboard/views/widgets/range_option_in_all_expenses.dart';
 
 class AllExpensesHeader extends StatelessWidget {
   const AllExpensesHeader({super.key});
@@ -6,7 +8,16 @@ class AllExpensesHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Row(
-      children: [],
+      children: [
+        Text(
+          'All Expenses',
+          style: AppStyles.styleSemiBold20,
+        ),
+        Expanded(
+          child: SizedBox(),
+        ),
+        RangeOptionInAllExpenses(),
+      ],
     );
   }
 }
