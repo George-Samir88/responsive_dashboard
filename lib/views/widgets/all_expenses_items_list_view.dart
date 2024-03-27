ceaef8c3797e0d0c31bc2892375ce54572dabc39
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-03-27 15:46:15 +0200

    all expenses item part 4

---
+++ lib/views/widgets/all_expenses_items_list_view.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/views/widgets/all_expenses_item.dart';
+
+import '../../generated/assets.dart';
+import '../../models/all_expenses_item_model.dart';
+
+class AllExpensesItemsListView extends StatelessWidget {
+  const AllExpensesItemsListView({super.key});
+
+  final List<AllExpensesItemModel> allExpensesItemModel = const [
+    AllExpensesItemModel(
+      image: Assets.imagesBalance,
+      title: 'Balance',
+      date: 'April 2022',
+      price: r'$20,129',
+    ),
+    AllExpensesItemModel(
+      image: Assets.imagesIncome,
+      title: 'Income',
+      date: 'April 2022',
+      price: r'$20,129',
+    ),
+    AllExpensesItemModel(
+      image: Assets.imagesExpenses,
+      title: 'Expenses',
+      date: 'April 2022',
+      price: r'$20,129',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      //asMap give you access to all entries in list and its key to control in each item by its index
+      children: allExpensesItemModel.asMap().entries.map((e) {
+        int index = e.key;
+        var value = e.value;
+        return Expanded(
+          child: Padding(
+            padding: index == 1
+                ? const EdgeInsets.symmetric(horizontal: 12)
+                : EdgeInsets.zero,
+            child: AllExpensesItem(allExpensesItemModel: value),
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
