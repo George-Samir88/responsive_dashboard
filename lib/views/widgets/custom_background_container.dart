771433fbdb889c1ce19afaa69679146695bcfd55
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-04-02 15:52:17 +0200

    create custom background container

---
+++ lib/views/widgets/custom_background_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomBackgroundContainer extends StatelessWidget {
+  const CustomBackgroundContainer({
+    super.key,
+    this.padding,
+    required this.child,
+  });
+
+  final double? padding;
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(padding ?? 20),
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: child,
+    );
+  }
+}
