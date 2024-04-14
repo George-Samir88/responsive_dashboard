913c774310fe64d22debf5c6b0f10812d2a2d2e2
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-04-14 16:01:04 +0200

    my card part 3

--- lib/views/widgets/my_card.dart
+++ lib/views/widgets/my_card.dart
@@ -21,6 +21,7 @@ class MyCard extends StatelessWidget {
           ),
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
               contentPadding: const EdgeInsets.only(
@@ -28,15 +29,37 @@ class MyCard extends StatelessWidget {
                 left: 31,
                 right: 42,
               ),
-              title: const Text(
+              title: Text(
                 'Name card',
-                style: AppStyles.styleRegular16,
+                style: AppStyles.styleRegular16.copyWith(color: Colors.white),
               ),
               subtitle: const Text(
                 'Syah Bandi',
                 style: AppStyles.styleMedium20,
               ),
               trailing: SvgPicture.asset(Assets.imagesGallery),
+            ),
+            const Expanded(child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    '0918 8124 0042 8129',
+                    style: AppStyles.styleSemiBold24,
+                  ),
+                  Text(
+                    '12/20 - 124',
+                    style: AppStyles.styleRegular16.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 54 - 30,
             ),
           ],
         ),
