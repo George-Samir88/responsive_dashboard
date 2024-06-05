921bc2ad9f25a6d282da90eeed92942bf920e2e0
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-06-05 15:50:04 +0300

    mobile layout done

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/views/adaptive_layout.dart';
+import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
 import 'package:responsive_dashboard/views/widgets/desktop_dashboard.dart';
 import 'package:responsive_dashboard/views/widgets/mobile_dashboard_layout.dart';
 import 'package:responsive_dashboard/views/widgets/tablet_dashboard_layout.dart';
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
+
+  @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       backgroundColor: const Color(0xffE5E5E5),
       appBar: MediaQuery.sizeOf(context).width < 800
           ? AppBar(
               backgroundColor: const Color(0xffFAFAFA),
-              leading: const Icon(Icons.menu),
+              leading: IconButton(
+                  onPressed: () {
+                    scaffoldKey.currentState!.openDrawer();
+                  },
+                  icon: const Icon(Icons.menu)),
             )
           : null,
+      drawer:
+          MediaQuery.sizeOf(context).width < 800 ? const CustomDrawer() : null,
       body: AdaptiveLayout(
           mobileLayout: (context) => const MobileDashboardLayout(),
           tabletLayout: (context) => const TabletDashboardLayout(),
